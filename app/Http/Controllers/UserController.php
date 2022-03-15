8b8cde332f97b103ee985d10ccbbdb6f0c32fa4c
Diego Batista <dgobatistam@outlook.com>
2022-03-15 16:13:07 -0300

    Aula 19 - OK

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,9 +9,16 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $users = User::get();
+
+        $search = $request->search;
+        $users = User::where(function ($query) use ($search) {
+            if ($search)
+            $query->where('email', $search);
+            $query->orWhere('name', 'LIKE', "%{$search}%");
+        })->get();
+
         return view('users.index', compact('users'));
     }
 
@@ -36,6 +43,30 @@ class UserController extends Controller
 
         return redirect()->route('users.index');
     }
+
+    public function edit($id)
+    {
+        if (!$user = User::find($id))
+             return redirect()->route('users.index'); 
+       
+             return view ('users.edit', compact('user'));
+    }
+
+
+    public function update(StoreUpdateUserFormRequest $request, $id)
+    {
+        if (!$user = User::find($id))
+             return redirect()->route('users.index'); 
+
+        $data = $request->only('name', 'email');
+        if ($request->password)
+            $data['password'] = bcrypt($request->password);
+       
+        $user->update($data);
+
+        return redirect()->route('users.index');
+    }
+
 }
 
 
